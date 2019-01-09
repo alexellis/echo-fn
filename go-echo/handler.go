b6bfed92e5f80737de261226622c91f2675cde18
Alex Ellis <alexellis2@gmail.com>
2019-01-09 22:24:47 +0000

    Initial
    Signed-off-by: Alex Ellis <alexellis2@gmail.com>

---
+++ go-echo/handler.go
@@ -0,0 +1,17 @@
+package function
+
+import (
+	"net/http"
+
+	"github.com/openfaas-incubator/go-function-sdk"
+)
+
+// Handle a function invocation
+func Handle(req handler.Request) (handler.Response, error) {
+	var err error
+
+	return handler.Response{
+		Body:       req.Body,
+		StatusCode: http.StatusOK,
+	}, err
+}
